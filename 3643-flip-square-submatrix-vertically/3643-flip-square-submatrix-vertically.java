762750727e275c04151041a6e9817ad04fda59ce
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-03-21 11:47:43 +0530

    Time: 0 ms (100%), Space: 47.5 MB (34.69%) - LeetHub

--- 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
+++ 3643-flip-square-submatrix-vertically/3643-flip-square-submatrix-vertically.java
@@ -1,14 +1,33 @@
+//self
+// class Solution {
+//     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         for(int i = x ; i < x + k / 2 ; i++){
+//             int l = x + k - 1 - (i - x); // took help here
+//             for(int j = y ; j < y + k ; j++){
+//                 int temp = grid[i][j];
+//                 grid[i][j] = grid[l][j];
+//                 grid[l][j] = temp;
+//             }
+//         }
+//         return grid;
+//     }
+// }
+
+//with help
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
-        for(int i = x ; i < x + k / 2 ; i++){
-            int l = x + k - 1 - (i - x);
-            for(int j = y ; j < y + k ; j++){
-                int temp = grid[i][j];
-                grid[i][j] = grid[l][j];
-                grid[l][j] = temp;
+        int top = x;
+        int bottom = x + k - 1;
+        while (top < bottom) {
+            for (int j = y; j < y + k; j++) {
+                int temp = grid[top][j];
+                grid[top][j] = grid[bottom][j];
+                grid[bottom][j] = temp;
             }
+            top++;
+            bottom--;
         }
         return grid;
     }
