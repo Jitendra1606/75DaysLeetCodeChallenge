b53ebc3bf7dc580f325bdcfd3cf02b3164f60507
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-03-26 19:51:45 +0530

    Time: 38 ms (11.34%), Space: 79.2 MB (11.73%) - LeetHub

---
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,42 @@
+//memoization
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for(int[] rows : dp){
+            Arrays.fill(rows, -1);
+        }
+        return solve(0, 1, n, prices, fee, dp);
+    }
+    public int solve(int ind, int buy, int n, int[] prices, int fee, int[][] dp){
+        if(ind == n) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        int profit = 0;
+        if(buy == 1){
+            profit = Math.max(-prices[ind] + solve(ind + 1, 0, n, prices, fee, dp), 0 + solve(ind + 1, 1, n, prices, fee, dp));
+        }else{
+            profit = Math.max(prices[ind] - fee + solve(ind + 1, 1, n, prices, fee, dp), 0 + solve(ind + 1, 0, n, prices, fee, dp));
+        }
+        dp[ind][buy] = profit;
+        return profit;
+    }
+}
+
+
+//recursive
+// class Solution {
+//     public int maxProfit(int[] prices, int fee) {
+//         int n = prices.length;
+//         return solve(0, 1, n, prices, fee);
+//     }
+//     public int solve(int ind, int buy, int n, int[] prices, int fee){
+//         if(ind == n) return 0;
+//         int profit = 0;
+//         if(buy == 1){
+//             profit = Math.max(-prices[ind] + solve(ind + 1, 0, n, prices, fee), 0 + solve(ind + 1, 1, n, prices, fee));
+//         }else{
+//             profit = Math.max(prices[ind] - fee + solve(ind + 1, 1, n, prices, fee), 0 + solve(ind + 1, 0, n, prices, fee));
+//         }
+//         return profit;
+//     }
+// }
