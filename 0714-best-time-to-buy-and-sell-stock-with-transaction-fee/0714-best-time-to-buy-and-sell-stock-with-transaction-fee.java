40a391293b58a371596ff5354c05ecd3ab23b76b
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-03-26 19:57:42 +0530

    Time: 19 ms (41%), Space: 70.6 MB (47.88%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,26 +1,46 @@
-//memoization
-class Solution {
-    public int maxProfit(int[] prices, int fee) {
+//bottom up
+class Solution{
+    public int maxProfit(int[] prices, int fee){
         int n = prices.length;
-        int[][] dp = new int[n][2];
-        for(int[] rows : dp){
-            Arrays.fill(rows, -1);
+        int[][] dp = new int[n + 1][2];
+        for(int ind = n - 1 ; ind >= 0 ; ind--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    profit = Math.max(-prices[ind] + dp[ind + 1][0], 0 + dp[ind + 1][1]);
+                }else{
+                    profit = Math.max(prices[ind] - fee + dp[ind + 1][1], 0 + dp[ind + 1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
         }
-        return solve(0, 1, n, prices, fee, dp);
-    }
-    public int solve(int ind, int buy, int n, int[] prices, int fee, int[][] dp){
-        if(ind == n) return 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        int profit = 0;
-        if(buy == 1){
-            profit = Math.max(-prices[ind] + solve(ind + 1, 0, n, prices, fee, dp), 0 + solve(ind + 1, 1, n, prices, fee, dp));
-        }else{
-            profit = Math.max(prices[ind] - fee + solve(ind + 1, 1, n, prices, fee, dp), 0 + solve(ind + 1, 0, n, prices, fee, dp));
-        }
-        dp[ind][buy] = profit;
-        return profit;
+        return dp[0][1];
     }
 }
+
+//memoization
+// class Solution {
+//     public int maxProfit(int[] prices, int fee) {
+//         int n = prices.length;
+//         int[][] dp = new int[n][2];
+//         for(int[] rows : dp){
+//             Arrays.fill(rows, -1);
+//         }
+//         return solve(0, 1, n, prices, fee, dp);
+//     }
+//     public int solve(int ind, int buy, int n, int[] prices, int fee, int[][] dp){
+//         if(ind == n) return 0;
+//         if(dp[ind][buy] != -1) return dp[ind][buy];
+//         int profit = 0;
+//         if(buy == 1){
+//             profit = Math.max(-prices[ind] + solve(ind + 1, 0, n, prices, fee, dp), 0 + solve(ind + 1, 1, n, prices, fee, dp));
+//         }else{
+//             profit = Math.max(prices[ind] - fee + solve(ind + 1, 1, n, prices, fee, dp), 0 + solve(ind + 1, 0, n, prices, fee, dp));
+//         }
+//         dp[ind][buy] = profit;
+//         return profit;
+//     }
+// }
 
 
 //recursive
