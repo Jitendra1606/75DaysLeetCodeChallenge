8bbd4b810eea51760750bd4e9d66938d9b9d8daf
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-03-30 08:55:17 +0530

    Time: 5 ms (98.68%), Space: 47.1 MB (100%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -4,22 +4,14 @@ class Solution {
         int[] freq1 = new int[26];
         int[] freq2 = new int[26];
         for(int i = 0 ; i < s1.length() ; i++){
-            char ch = s1.charAt(i);
+            char ch1 = s1.charAt(i);
+            char ch2 = s2.charAt(i);
             if(i % 2 == 0){
-                freq1[ch - 'a']++;
+                freq1[ch1 - 'a']++;
+                freq1[ch2 - 'a']--;
             }else{
-                freq2[ch - 'a']++;
-            }
-        }
-        for(int i = 0 ; i < s2.length() ; i++){
-            char ch = s2.charAt(i);
-            if(i % 2 == 0){
-                freq1[ch - 'a']--;
-            }else{
-                freq2[ch - 'a']--;
-            }
-            if(freq1[ch - 'a'] < 0 || freq2[ch - 'a'] < 0){
-                return false;
+                freq2[ch1 - 'a']++;
+                freq2[ch2 - 'a']--;
             }
         }
         for(int i = 0 ; i < 26 ; i++){
@@ -28,3 +20,34 @@ class Solution {
         return true;
     }
 }
+
+// class Solution {
+//     public boolean checkStrings(String s1, String s2) {
+//         if(s1.equals(s2)) return true;
+//         int[] freq1 = new int[26];
+//         int[] freq2 = new int[26];
+//         for(int i = 0 ; i < s1.length() ; i++){
+//             char ch = s1.charAt(i);
+//             if(i % 2 == 0){
+//                 freq1[ch - 'a']++;
+//             }else{
+//                 freq2[ch - 'a']++;
+//             }
+//         }
+//         for(int i = 0 ; i < s2.length() ; i++){
+//             char ch = s2.charAt(i);
+//             if(i % 2 == 0){
+//                 freq1[ch - 'a']--;
+//             }else{
+//                 freq2[ch - 'a']--;
+//             }
+//             if(freq1[ch - 'a'] < 0 || freq2[ch - 'a'] < 0){
+//                 return false;
+//             }
+//         }
+//         for(int i = 0 ; i < 26 ; i++){
+//             if(freq1[i] != 0 || freq2[i] != 0) return false;
+//         }
+//         return true;
+//     }
+// }
