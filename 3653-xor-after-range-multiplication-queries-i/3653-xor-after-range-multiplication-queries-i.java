b95abaec4279f786c180d88d5a6ac4c79a8f0a2a
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-04-08 07:43:27 +0530

    Time: 59 ms (88%), Space: 47.3 MB (97.33%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    static final int M = 1_000_000_007;
+
+    public int xorAfterQueries(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int q = queries.length;
+
+        long[] arr = new long[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = nums[i] ;
+        }
+        for(int i = 0 ; i < q ; i++){
+            int idx = queries[i][0];
+
+            while(idx <= queries[i][1]){
+                arr[idx] = (arr[idx] * queries[i][3]) % M;
+                idx += queries[i][2];
+            }
+        }
+
+        int xor = 0;
+        for(int j = 0 ; j < n ; j++){
+            xor = xor ^ (int)arr[j];
+        }
+        return xor;
+    }
+}
