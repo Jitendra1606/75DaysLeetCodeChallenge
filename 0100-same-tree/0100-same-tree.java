cb2363035562c298c46d6586a408c4881eca7390
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-04-16 07:42:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (98.32%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,37 +14,28 @@
  * }
  */
 
-
 class Solution{
     public boolean isSameTree(TreeNode p, TreeNode q){
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
-        if(p.val != q .val) return false;
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        
+        if(p.val != q.val) return false;
+        if(!isSameTree(p.left, q.left)) return false;
+        if(!isSameTree(p.right, q.right)) return false;
+        return true;
     }
 }
-// class Solution {
-//     List<Integer> list1 = new ArrayList<>();
-//     List<Integer> list2 = new ArrayList<>();
-//     public boolean isSameTree(TreeNode p, TreeNode q) {
-//         inorder1(p);
-//         inorder2(q);
-//         if(list1.size() != list2.size()) return false;
-//         for(int i = 0 ; i < list1.size() ; i++){
-//             if(list1.get(i) != list2.get(i)) return false;
-//         }
-//         return true;
-//     }
-//     public void inorder1(TreeNode node){
-//         if(node == null) return;
-//         inorder1(node.left);
-//         list1.add(node.val);
-//         inorder1(node.right);
-//     }
-//     public void inorder2(TreeNode node){
-//         if(node == null) return;
-//         inorder2(node.left);
-//         list2.add(node.val);
-//         inorder2(node.right);
+
+
+// class Solution{
+//     public boolean isSameTree(TreeNode p, TreeNode q){
+//         //when both null
+//         if(p == null && q == null) return true;
+//         //when either of one is not null
+//         if(p == null || q == null) return false;
+//         //checking values, that must be same
+//         if(p.val != q .val) return false;
+//         //moving forward
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 //     }
 // }
