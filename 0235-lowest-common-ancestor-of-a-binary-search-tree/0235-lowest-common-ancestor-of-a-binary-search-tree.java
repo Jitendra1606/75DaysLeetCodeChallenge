f02f6a1e3142a09338cd015d025af54aa387ad7e
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-04-20 09:13:29 +0530

    Time: 6 ms (97.18%), Space: 47.8 MB (20.61%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -7,19 +7,38 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
-class Solution {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        int current = root.val;
-        if(current < p.val && current < q.val){
-            return lowestCommonAncestor(root.right, p, q);
+//simple while loop which does not take any space
+class Solution{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
+        while(root != null){
+            if(root.val > p.val && root.val > q.val){
+                root = root.left;
+            }
+            else if(root.val < p.val && root.val < q.val){
+                root = root.right;
+            }
+            else{
+                return root;
+            }
         }
-        else if(current > p.val && current > q.val){
-            return lowestCommonAncestor(root.left, p, q);
-        }
-        else{
-            return root;
-        }
+        return null;
     }
 }
+
+
+//this is recursive approach which takes extra recursive stack  space
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null) return null;
+//         int current = root.val;
+//         if(current < p.val && current < q.val){
+//             return lowestCommonAncestor(root.right, p, q);
+//         }
+//         else if(current > p.val && current > q.val){
+//             return lowestCommonAncestor(root.left, p, q);
+//         }
+//         else{
+//             return root;
+//         }
+//     }
+// }
