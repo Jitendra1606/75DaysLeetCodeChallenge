d391aea5fbd20489be0ed23f3ba978babc038445
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-09 09:13:20 +0530

    Time: 54 ms (67.44%), Space: 83.3 MB (35.94%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minJumps(int[] arr) {
+
+        int n = arr.length, cnt = 0;
+
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        for(int i = 0 ; i < n ; i++){
+            map.putIfAbsent(arr[i], new ArrayList<>());
+            map.get(arr[i]).add(i);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(0);
+        visited[0] = true;
+
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+
+            while(size-- > 0){
+
+                int i = queue.poll();
+
+                if(i == n - 1) return cnt;
+
+                //i - 1
+                if(i - 1 >= 0 && !visited[i - 1]){
+                    queue.offer(i - 1);
+                    visited[i - 1] = true;
+                }
+
+                //i + 1
+                if(i + 1 < n && !visited[i + 1]){
+                    queue.offer(i + 1);
+                    visited[i + 1] = true;
+                }
+                
+                //a[i] = a[j] , i != j
+                if(map.containsKey(arr[i]) && !set.contains(arr[i])){
+                    for(int j : map.get(arr[i])){
+                        if(!visited[j]){
+                            visited[j] = true;
+                            queue.offer(j);
+                        }
+                    }
+                    set.add(arr[i]);
+                }
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+}
