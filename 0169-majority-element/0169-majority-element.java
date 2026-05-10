88875edf8402bc9355b85ef1223dfb17e2011cea
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-10 14:09:46 +0530

    Time: 1 ms (99.85%), Space: 55.6 MB (59.86%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,30 +1,35 @@
-// class Solution {
-//     public int majorityElement(int[] nums) {
+//moore's voting algorithm
+class Solution{
+    public int majorityElement(int[] nums){
+        int n = nums.length;
+        int cnt = 0, element = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(cnt == 0){
+                cnt = 1;
+                element = nums[i];
+            }else if(nums[i] == element){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        return element;
+    }
+}
+//t.c = O(n)
+//s.c = O(1)
+
+// class Solution{
+//     public int majorityElement(int[] nums){
 //         int n = nums.length;
-//         int div = n / 2;
-//         HashMap<Integer , Integer> map = new HashMap<>();
+//         int max = n / 2;
+//         HashMap<Integer, Integer> map = new HashMap<>();
 //         for(int i = 0 ; i < n ; i++){
-//             map.put(nums[i],map.getOrDefault(nums[i] , 0) + 1);
-            
-//             if(map.get(nums[i]) > div){
-//             return nums[i];
+//             map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
+//             if(map.get(nums[i]) > max) return nums[i];
 //         }
-//         }
-       
 //         return -1;
 //     }
 // }
-class Solution {
-    public int majorityElement(int[] nums) {
-        int count = 0;
-        int candidate = 0;
-
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
-            }
-            count += (num == candidate ? 1 : -1);
-        }
-        return candidate;
-    }
-}
+//t.c = O(n)
+//s.c = O(n) (for hashmap)
