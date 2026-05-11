5c2e98d5e62c695892f1b1c25bdc36399970d57f
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-11 13:59:44 +0530

    Time: 25 ms (43.97%), Space: 48.9 MB (35.09%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,67 +1,32 @@
 class Solution{
     public int subarraySum(int[] nums, int k){
-        int n = nums.length, cnt = 0;
+
+        int n = nums.length, sum = 0, cnt = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); //till now sum 0 has comes 1 time
+
         for(int i = 0 ; i < n ; i++){
-            int sum = 0;
-            for(int j = i ; j < n ; j++){
-                sum += nums[j];
-                if(sum == k){
-                    cnt++;
-                }
+            sum += nums[i];
+
+            if(map.containsKey(sum - k)){
+                cnt += map.get(sum - k);
             }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
+        
         return cnt;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Solution{
-//     public int subarraySum(int[] nums, int k){
-//         int n = nums.length;
-//         int sum = 0, cnt = 0;
-
-//         Map<Integer, Integer> map = new HashMap<>();
-//         map.put(0, 1); //sum = 0, comes 1 time till now
-
-//         for(int i = 0 ; i < n ; i++){
-//             sum += nums[i];
-
-//             if(map.containsKey(sum - k)){
-//                 cnt += map.get(sum - k);
-//             }
-//             map.put(sum, map.getOrDefault(sum, 0) + 1);
-//         }
-//         return cnt;
-//     }
-// }
+//t.c = O(n)
+//s.c = O(n)(hashmap)
 
 
 //brute force
-// class Solution {
-//     public int subarraySum(int[] nums, int k) {
-//         int n = nums.length;
-//         int cnt = 0;
+// class Solution{
+//     public int subarraySum(int[] nums, int k){
+//         int n = nums.length, cnt = 0;
 //         for(int i = 0 ; i < n ; i++){
 //             int sum = 0;
 //             for(int j = i ; j < n ; j++){
@@ -74,3 +39,5 @@ class Solution{
 //         return cnt;
 //     }
 // }
+//t.c = O(n * n)
+//s.c = O(1)
