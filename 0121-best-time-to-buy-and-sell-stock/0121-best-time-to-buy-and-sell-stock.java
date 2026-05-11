033d8fc0f8f3affb8404ec2b8928bec3565ee064
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-11 09:33:10 +0530

    Time: 2 ms (39.2%), Space: 94.4 MB (69.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,88 +1,34 @@
+//t.c = O(n), s.c = O(1)
 class Solution{
     public int maxProfit(int[] prices){
-        int i = 0, j = 1;
-        int max = 0;
+        int n = prices.length;
         int min = prices[0];
-        while(j < prices.length){
-            if(prices[j] < prices[i]){
-                min = prices[j];
-                i = j;
-            }else{//prices[j] > prices[i]
-                max = Math.max(max, prices[j] - prices[i]);
-            }
-            j++;
+        int profit = 0, cost = 0;
+        for(int i = 1 ; i < n ; i++){
+            cost = prices[i] - min;
+            profit = Math.max(cost, profit);
+            min = Math.min(min, prices[i]);
         }
-        return max;
+        return profit;
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//all the solutions below are of two pointers approach
 // class Solution{
 //     public int maxProfit(int[] prices){
-//         int min = Integer.MAX_VALUE;
-//         int maxprofit = 0;
-//         for(int i = 0 ; i < prices.length ; i++){
-//             if(min > prices[i]){
-//                 min = prices[i];
-//             }
-//             else{
-//                 maxprofit = Math.max(maxprofit , prices[i] - min);
-//             }
-//         }
-//         return maxprofit;
-//     }
-// }
-
-
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int min = prices[0];
-//         int maxprofit = 0;
-//         int n = prices.length;
-//         for(int i = 1 ; i < n ; i++){
-//             int cost = prices[i] - min;
-//             maxprofit = Math.max(maxprofit , cost);
-//             min = Math.min(min , prices[i]);
-//         }
-//         return maxprofit;
-//     }
-// }
-
-
-
-// class Solution{
-//     public int maxProfit(int[] prices){
-//         int n = prices.length;
+//         int i = 0, j = 1;
 //         int max = 0;
 //         int min = prices[0];
-//         for(int i = 1 ; i < n ; i++){
-//             int cost = prices[i] - min;
-//             max = Math.max(cost , max);
-//             min = Math.min(min , prices[i]);
+//         while(j < prices.length){
+//             if(prices[j] < prices[i]){
+//                 min = prices[j];
+//                 i = j;
+//             }else{//prices[j] > prices[i]
+//                 max = Math.max(max, prices[j] - prices[i]);
+//             }
+//             j++;
 //         }
 //         return max;
 //     }
 // }
-
-
-
-
