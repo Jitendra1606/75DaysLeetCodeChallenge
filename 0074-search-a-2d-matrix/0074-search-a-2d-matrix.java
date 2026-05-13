fe0749953f52f968e993e20e2af1ff7d2bb18107
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-13 16:47:29 +0530

    Time: 0 ms (100%), Space: 43.9 MB (60.87%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,18 +1,50 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int[] res = new int[n * m];
-        for(int i = 0 ; i < n * m ; i++){
-            res[i] = matrix[i / m][i % m];
+class Solution{
+    public boolean searchMatrix(int[][] matrix, int target){
+        int n = matrix.length, m = matrix[0].length;
+
+        int low = 0, high = n * m - 1;
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            int element = matrix[mid / m][mid % m];
+
+            if(element == target) return true;
+
+            else if(element < target) low = mid + 1;
+
+            else high = mid - 1;
         }
-        int start = 0, end = res.length - 1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(res[mid] == target) return true;
-            else if(res[mid] < target) start = mid + 1;
-            else end = mid - 1;
-        }
+
         return false;
     }
 }
+//t.c = O(log mn)
+//s.c = O(1)
+
+// class Solution{
+//     public boolean searchMatrix(int[][] matrix, int target){
+//         int n = matrix.length, m = matrix[0].length;
+
+//         int[] res = new int[m * n];
+//         for(int i = 0 ; i < m * n ; i++){
+//             res[i] = matrix[i / m][i % m];
+//         } 
+
+//         int low = 0, high = m * n - 1;
+//         while(low <= high){
+
+//             int mid = low + (high - low) / 2;
+
+//             if(res[mid] == target) return true;
+
+//             else if(res[mid] < target) low = mid + 1;
+
+//             else high = mid - 1;
+//         }
+//         return false;
+//     }
+// }
+//t.c = O(m * n) + O(log m*n)
+//s.c = O(m * n)
