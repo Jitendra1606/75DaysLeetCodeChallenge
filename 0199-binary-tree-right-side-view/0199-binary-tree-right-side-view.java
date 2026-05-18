9a259e5b382277b25db397bf4cdc12fe02964b08
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-05-18 09:15:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (82.5%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,40 +13,57 @@
  *     }
  * }
  */
-class Pair{
-    TreeNode node;
-    int level;
-
-    Pair(TreeNode node, int level){
-        this.node = node;
-        this.level = level;
-    }
-}
-
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        if(root == null) return list;
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(root, 0));
-
-        while(!q.isEmpty()){
-            Pair cur = q.poll();
-            TreeNode node = cur.node;
-            int hd = cur.level;
-
-            map.put(hd, node.val);
-            if(node.left != null){
-                q.offer(new Pair(node.left, hd + 1));
-            }
-            if(node.right != null){
-                q.offer(new Pair(node.right, hd + 1));
-            }
-        } 
-        for(Integer val : map.values()){
-            list.add(val);
-        }
+class Solution{
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> rightSideView(TreeNode root){
+        right(root, 0);
         return list;
     }
+    public void right(TreeNode node, int level){
+        if(node == null) return;
+        if(level == list.size()) list.add(node.val);
+
+        right(node.right, level + 1);
+        right(node.left, level + 1);
+    }
 }
+
+
+//iterative approach
+// class Pair{
+//     TreeNode node;
+//     int level;
+
+//     Pair(TreeNode node, int level){
+//         this.node = node;
+//         this.level = level;
+//     }
+// }
+
+// class Solution {
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         if(root == null) return list;
+//         TreeMap<Integer, Integer> map = new TreeMap<>();
+//         Queue<Pair> q = new LinkedList<>();
+//         q.offer(new Pair(root, 0));
+
+//         while(!q.isEmpty()){
+//             Pair cur = q.poll();
+//             TreeNode node = cur.node;
+//             int hd = cur.level;
+
+//             map.put(hd, node.val);
+//             if(node.left != null){
+//                 q.offer(new Pair(node.left, hd + 1));
+//             }
+//             if(node.right != null){
+//                 q.offer(new Pair(node.right, hd + 1));
+//             }
+//         } 
+//         for(Integer val : map.values()){
+//             list.add(val);
+//         }
+//         return list;
+//     }
+// }
