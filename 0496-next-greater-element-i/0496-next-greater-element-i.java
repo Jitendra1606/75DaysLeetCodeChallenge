4e8f5c6c65f96988ffd51f6f123e2b208ede8c24
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-06 13:52:40 +0530

    Time: 4 ms (75.42%), Space: 45.4 MB (21.8%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,29 @@
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//instead of creating a new array to store next greater element of array we can directly store this data in hashmap as we have to use this data to answer the next greater element in nums2 corresponding to nums1
 
-        Map<Integer, Integer> map = new HashMap<>();
+class Solution{
+    public int[] nextGreaterElement(int[] nums1, int[] nums2){
+        int n = nums1.length;
+
         Stack<Integer> st = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = nums2.length - 1 ; i >= 0 ; i--){
-
+        for(int i = nums2.length - 1 ; i >= 0 ;i--){
             while(!st.isEmpty() && st.peek() <= nums2[i]){
                 st.pop();
             }
-
-            int nextGr = st.isEmpty() ? -1 : st.peek();
-
-            map.put(nums2[i], nextGr);
+            int next_gtr = st.isEmpty() ? -1 : st.peek();
+            
+            map.put(nums2[i], next_gtr);
 
             st.push(nums2[i]);
         }
+        int[] ans = new int[n];
 
-        int[] ans = new int[nums1.length];
-
-        for(int i = 0 ; i < nums1.length ; i++){
+        for(int i = 0 ; i < n ; i++){
             ans[i] = map.get(nums1[i]);
         }
         return ans;
     }
 }
+//t.c = O(n)
+//s.c = O(n)
