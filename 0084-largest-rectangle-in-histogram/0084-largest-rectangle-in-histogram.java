7affc08c8d3c98bbb9421e2461a8b4fd90ebc06f
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-08 15:12:36 +0530

    Time: 105 ms (29.84%), Space: 82.8 MB (12.61%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,58 +1,40 @@
-class Solution {
-    public int largestRectangleArea(int[] heights) {
+//going with concept of nsl(next smaller to left) and next smaller to right and * by width
+class Solution{
+    public int largestRectangleArea(int[] heights){
         int n = heights.length;
-       int[] nsr = new int[n];
-       int[] nsl = new int[n];
-       int[] width = new int[n];
-       Stack<Integer> s1 = new Stack<>();
-       Stack<Integer> s2 = new Stack<>();
-       for(int i = n - 1 ; i >= 0 ; i--){
-        if(s1.isEmpty()){
-            nsr[i] = n;
-        }
-        else if(!s1.isEmpty() && heights[s1.peek()] < heights[i]){
-            nsr[i] = s1.peek();
-        }
-        else if(!s1.isEmpty() && heights[s1.peek()] >= heights[i]){
-            while(!s1.isEmpty() && heights[s1.peek()] >= heights[i]){
-                s1.pop();
+
+        Stack<Integer> st = new Stack<>();
+
+        int[] nsl = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
+                st.pop();
             }
-            if(s1.isEmpty()){
-                nsr[i] = n;
-            }
-            else{
-                nsr[i] = s1.peek();
-            }
+
+            nsl[i] = st.isEmpty() ? -1 : st.peek();
+
+            st.push(i);
         }
-        s1.push(i);
-       } 
-       for(int i = 0 ; i < n ; i++){
-        if(s2.isEmpty()){
-            nsl[i] = -1;
-        }
-        else if(!s2.isEmpty() && heights[s2.peek()] < heights[i]){
-            nsl[i] = s2.peek();
-        }
-        else if(!s2.isEmpty() && heights[s2.peek()] >= heights[i]){
-            while(!s2.isEmpty() && heights[s2.peek()] >= heights[i]){
-                s2.pop();
+
+        st.clear();
+
+        int[] nsr = new int[n];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            while(!st.isEmpty() && heights[st.peek()] >= heights[i]){
+                st.pop();
             }
-            if(s2.isEmpty()){
-                nsl[i] = -1;
-            }
-            else{
-                nsl[i] = s2.peek();
-            }
+
+            nsr[i] = st.isEmpty() ? n : st.peek();
+
+            st.push(i);
         }
-        s2.push(i);
-       } 
-       for(int i = 0 ; i < n ; i++){
-        width[i] = nsr[i] - nsl[i] - 1;
-       }
-       int max = 0;
-       for(int i = 0 ; i < n ; i++){
-        max = Math.max(max , heights[i] * width[i]);
-       }
-       return max;
+
+        int[] width = new int[n];
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            width[i] = nsr[i] - nsl[i] - 1;
+            ans = Math.max(ans, heights[i] * width[i]);
+        }
+        return ans;
     }
 }
