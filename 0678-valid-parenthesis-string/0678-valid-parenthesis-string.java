df1172d78f62e254de25c7221239923a11caaec4
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-22 17:38:00 +0530

    Time: 0 ms (100%), Space: 42.5 MB (81.8%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,23 +1,38 @@
 class Solution {
     public boolean checkValidString(String s) {
         int low = 0, high = 0;
-        for(int i = 0 ; i < s.length() ; i++){
-            char ch = s.charAt(i);
-            if(ch == '('){
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
                 low++;
                 high++;
-            }
-            else if(ch == ')'){
+            } else if (c == ')') {
                 low--;
                 high--;
+            } else {
+                low--;
+                high++;
             }
-            else{
-                low--; // assuming here ')'
-                high++; // assuming here '('
-            }
-            if(high < 0) return false;
-            if(low < 0) low = 0;
+            if (low < 0)
+                low = 0;
+            if (high < 0)
+                return false;
         }
         return low == 0;
     }
 }
+
+// low = minimum possible number of unmatched '('
+// high = maximum possible number of unmatched '('
+
+// Think of low and high as:
+
+// low  = best case (fewest unmatched '(')
+// high = worst case (most unmatched '(')
+
+// For '*':
+
+// Treat as ')' -> low decreases
+// Treat as '(' -> high increases
