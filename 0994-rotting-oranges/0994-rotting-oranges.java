7fbda441f4fab3283eda833572269b0961ebca2d
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-29 17:17:20 +0530

    Time: 2 ms (90.73%), Space: 44.2 MB (39.98%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,51 +1,67 @@
-class Tuple{
-    int row;
-    int col;
-    int tm;
-
-    Tuple(int r, int c, int t){
-        row = r;
-        col = c;
-        tm = t;
-    }
-}
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
+
         Queue<Tuple> q = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
+
+        boolean[][] visited = new boolean[n][m];
         int cntFresh = 0;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(grid[i][j] == 2){
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (grid[i][j] == 2) {
                     q.offer(new Tuple(i, j, 0));
                     visited[i][j] = true;
-                } 
-                if(grid[i][j] == 1) cntFresh++;
+                }
+                if (grid[i][j] == 1)
+                    cntFresh++;
             }
-        }       
-        int[] drow = {-1, 0, 1, 0};
-        int[] dcol = {0, 1, 0, -1};
-        int tm = 0;
+        }
+
+        int[] drow = { -1, 0, 1, 0 };
+        int[] dcol = { 0, 1, 0, -1 };
+
+        int time = 0;
         int cnt = 0;
-        while(!q.isEmpty()){
+
+        while (!q.isEmpty()) {
+
             int r = q.peek().row;
             int c = q.peek().col;
-            int t = q.peek().tm;
+            int t = q.peek().tim;
             q.poll();
-            tm = Math.max(tm, t);
-            for(int i = 0; i < 4 ; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] == 1){
-                    q.offer(new Tuple(nrow, ncol, t + 1));
-                    visited[nrow][ncol] = true;
+
+            time = Math.max(time, t);
+
+            for (int i = 0; i < 4; i++) {
+
+                int newr = drow[i] + r;
+                int newc = dcol[i] + c;
+                //checking all 4 possibilities
+                if (newr >= 0 && newc >= 0 && newr < n && newc < m && !visited[newr][newc] && grid[newr][newc] == 1) {
+
+                    q.offer(new Tuple(newr, newc, t + 1)); //adjacent 1's are pushed in
+                    visited[newr][newc] = true;//the queue simultaneously at the same time
                     cnt++;
                 }
             }
         }
-        if(cnt != cntFresh) return -1;
-        return tm;
+        if (cntFresh != cnt)
+            return -1; //means all oranges doesn't become rotten
+        return time;
+    }
+}
+
+class Tuple {
+    int row;
+    int col;
+    int tim;
+
+    Tuple(int r, int c, int t) {
+        row = r;
+        col = c;
+        tim = t;
     }
 }
