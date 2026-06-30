3713b2170e73984d7497f40127d46b535c344e99
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-30 09:28:25 +0530

    Time: 2 ms (11.85%), Space: 46.8 MB (43.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,76 +1,81 @@
-class Solution{
-    int[] drow = {-1, 0, 1, 0};
-    int[] dcol = {0, 1, 0, -1};
-    int[][] ans;
-    int m, n;
-        public int[][] floodFill(int[][] image, int sr, int sc, int color){
-        n = image.length;
-        m = image[0].length;
+// class Solution{
+//     int[] drow = {-1, 0, 1, 0};
+//     int[] dcol = {0, 1, 0, -1};
+//     int n, m;
+//     int[][] ans;
 
-        int oldColor = image[sr][sc];
-        if(oldColor == color) return image;
-
-        ans = image;
-        dfs(image, sr, sc, oldColor, color);
-        return ans;
-    }
-
-    public void dfs(int[][] image, int row, int col, int old, int newColor){
-        ans[row][col] = newColor;
-
-        for(int i = 0 ; i < 4 ; i++){
-            int newr = drow[i] + row;
-            int newc = dcol[i] + col;
-
-            if(newr >= 0 && newc >= 0 && newr < n && newc < m && image[newr][newc] == old && ans[newr][newc] != newColor){
-                dfs(image, newr, newc, old, newColor);
-            }
-        }
-    }
-}
-
-
-//BFS is slight slow compared to DFS
-// class Pair{
-//     int row;
-//     int col;
-//     Pair(int r, int c){
-//         row = r;
-//         col = c;
-//     }
-// }
-
-// class Solution {
-//     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-//         int m = image.length;
-//         int n = image[0].length;
-//         boolean[][] visited = new boolean[m][n];
+//     public int[][] floodFill(int[][] image, int sr, int sc, int color){
+//         n = image.length;
+//         m = image[0].length;
 
 //         int oldColor = image[sr][sc];
 //         if(oldColor == color) return image;
 
-//         Queue<Pair> q = new LinkedList<>();
-//         q.offer(new Pair(sr, sc));
-//         image[sr][sc] = color;
-//         visited[sr][sc] = true;
+//         ans = image;
+//         dfs(image, sr, sc, color, oldColor);
+//         return ans;
+//     }
 
-//         int[] dRow = {-1, 0, 1, 0};
-//         int[] dCol = {0, 1, 0, -1};
-//         while(!q.isEmpty()){
-//             int r = q.peek().row;
-//             int c = q.peek().col;
-//             q.poll();
-//             for(int i = 0 ; i < 4 ; i++){
-//                 int nRow = r + dRow[i];
-//                 int nCol = c + dCol[i];
+//     public void dfs(int[][] image, int row, int col, int color, int oldColor){
+//         ans[row][col] = color;
 
-//                 if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && !visited[nRow][nCol] && image[nRow][nCol] == oldColor){
-//                     image[nRow][nCol] = color;
-//                     q.offer(new Pair(nRow, nCol));
-//                     visited[nRow][nCol] = true;
-//                 }
-//             }
+//         for(int i = 0 ; i < 4 ; i++){
+//             int newr = drow[i] + row;
+//             int newc = dcol[i] + col;
+
+//             if(newr < n && newc < m && newr >= 0 && newc >= 0 && image[newr][newc] == oldColor && ans[newr][newc] != color) dfs(image, newr, newc, color, oldColor);
 //         }
-//         return image;
 //     }
 // }
+
+
+
+//BFS is slight slow compared to DFS
+class Solution{
+    public int[][] floodFill(int[][] image, int sr, int sc, int color){
+        int n = image.length;
+        int m = image[0].length;
+
+        int oldColor = image[sr][sc];
+        if(oldColor == color) return image;
+
+        boolean[][] visited = new boolean[n][m];
+
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(sr, sc));
+
+        visited[sr][sc] = true;
+        image[sr][sc] = color;
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+
+        while(!q.isEmpty()){
+            int r = q.peek().row;
+            int c = q.peek().col;
+            q.poll();
+
+            for(int i = 0 ; i < 4 ; i++){
+                int newr = drow[i] + r;
+                int newc = dcol[i] + c;
+
+                if(newr < n && newc < m && newr >= 0 && newc >= 0 && !visited[newr][newc] && image[newr][newc] == oldColor){
+                    image[newr][newc] = color;
+                    visited[newr][newc] = true;
+                    q.offer(new Pair(newr, newc));
+                }
+            }
+        }
+        return image;
+    }
+}
+
+class Pair{
+    int row;
+    int col;
+
+    Pair(int r, int c){
+        row = r;
+        col = c;
+    }
+}
