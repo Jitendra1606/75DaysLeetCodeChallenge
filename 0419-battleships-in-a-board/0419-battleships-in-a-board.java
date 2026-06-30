42fd86172da3f83051f03fd1e4cbf6aeeb917f1b
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-06-30 09:28:54 +0530

    Time: 1 ms (98.6%), Space: 47.9 MB (7.61%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,34 +1,151 @@
-class Solution {
-    public int countBattleships(char[][] board) {
+class Solution{
+    public int countBattleships(char[][] board){
         int n = board.length;
         int m = board[0].length;
-        int cnt = 0;
 
         boolean[][] visited = new boolean[n][m];
 
+        int ships = 0;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
                 if(board[i][j] == 'X' && !visited[i][j]){
-                    cnt++;
-
-                    dfs(i, j, visited, board);
+                    ships++;
+                    dfs(i, j, board, visited);
                 }
             }
         }
-        return cnt;
+        return ships;
     }
 
-    public void dfs(int r, int c, boolean[][] visited, char[][] board){
-        visited[r][c] = true;
+    public void dfs(int row, int col, char[][] board, boolean[][] visited){
+        visited[row][col] = true;
 
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
 
         for(int i = 0 ; i < 4 ; i++){
-            int newr = drow[i] + r;
-            int newc = dcol[i] + c;
+            int newr = drow[i] + row;
+            int newc = dcol[i] + col;
 
-            if(newr >= 0 && newr < board.length && newc >= 0 && newc < board[0].length && !visited[newr][newc] && board[newr][newc] == 'X') dfs(newr, newc, visited, board);
+            if(newr < board.length && newc < board[0].length && newr >= 0 && newc >= 0 && !visited[newr][newc] && board[newr][newc] == 'X') 
+            dfs(newr, newc, board, visited);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int countBattleships(char[][] board) {
+//         int n = board.length;
+//         int m = board[0].length;
+//         int cnt = 0;
+
+//         boolean[][] visited = new boolean[n][m];
+
+//         for(int i = 0 ; i < n ; i++){
+//             for(int j = 0 ; j < m ; j++){
+//                 if(board[i][j] == 'X' && !visited[i][j]){
+//                     cnt++;
+
+//                     dfs(i, j, visited, board);
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+
+//     public void dfs(int r, int c, boolean[][] visited, char[][] board){
+//         visited[r][c] = true;
+
+//         int[] drow = {-1, 0, 1, 0};
+//         int[] dcol = {0, 1, 0, -1};
+
+//         for(int i = 0 ; i < 4 ; i++){
+//             int newr = drow[i] + r;
+//             int newc = dcol[i] + c;
+
+//             if(newr >= 0 && newr < board.length && newc >= 0 && newc < board[0].length && !visited[newr][newc] && board[newr][newc] == 'X') dfs(newr, newc, visited, board);
+//         }
+//     }
+// }
+
+
+
+
+
+// class Solution{
+//     public int countBattleships(char[][] board){
+//         int n = board.length;
+//         int m = board[0].length;
+
+//         boolean[][] visited = new boolean[n][m];
+//         int cnt = 0;
+
+//         int[] drow = {-1, 0, 1, 0};
+//         int[] dcol = {0, 1, 0, -1};
+
+//         for(int i = 0 ; i < n ; i++){
+
+//             for(int j = 0 ; j < m ; j++){
+
+//                 if(board[i][j] == 'X' && !visited[i][j]){
+//                     cnt++;
+
+//                     Queue<int[]> q = new LinkedList<>();
+
+//                     q.offer(new int[]{i, j});
+//                     visited[i][j] = true;
+
+//                     while(!q.isEmpty()){
+//                         int[] curr = q.poll();
+
+//                         int r = curr[0];
+//                         int c = curr[1];
+
+//                         for(int k = 0 ; k < 4 ; k++){
+
+//                             int newr = r + drow[k];
+//                             int newc = c + dcol[k];
+
+//                             if(newr >= 0 && newc >= 0 && newr < n && newc < m && board[newr][newc] == 'X' && !visited[newr][newc]){
+//                                 q.offer(new int[]{newr, newc});
+//                                 visited[newr][newc] = true;
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return cnt;
+//     } 
+// }
