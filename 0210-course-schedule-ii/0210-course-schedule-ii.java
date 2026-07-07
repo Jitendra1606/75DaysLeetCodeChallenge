39cb7ef3f752751de48b18b82cb3d5e2d4253827
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-07 17:22:18 +0530

    Time: 8 ms (17.5%), Space: 46.6 MB (90.79%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,40 +1,48 @@
-class Solution {
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // creating adjacency list
+class Solution{
+    public int[] findOrder(int numCourses, int[][] prerequisites){
+        
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++){
+
+        for(int i = 0 ; i < numCourses ; i++){
             adj.add(new ArrayList<>());
         }
-        for(int i = 0; i < prerequisites.length; i++){
-            int a = prerequisites[i][0];
-            int b = prerequisites[i][1];
-            // b -> a
-            adj.get(b).add(a);
-        }
+
+        for(int[] edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(v).add(u);
+        }       
+
         int[] indegree = new int[numCourses];
-        for(int i = 0; i < numCourses; i++){
+        for(int i = 0 ; i < numCourses ; i++){
             for(Integer it : adj.get(i)){
                 indegree[it]++;
             }
-        }
+        } 
+
         Queue<Integer> q = new LinkedList<>();
+
         for(int i = 0 ; i < numCourses ; i++){
             if(indegree[i] == 0) q.offer(i);
         }
+        
         int[] ans = new int[numCourses];
-        int i = 0;
+
+        int cnt = 0;
         while(!q.isEmpty()){
-            int node = q.poll();
-            ans[i++] = node;
+            int node = q.peek();
+            q.poll();
+
+            ans[cnt++] = node;
+
             for(Integer it : adj.get(node)){
                 indegree[it]--;
-                if(indegree[it] == 0) q.offer(it);
+
+                if(indegree[it] == 0) q.offer(it); 
             }
         }
-        if(i == numCourses){
-            return ans;
-        }else{
-            return new int[]{};
-        }
+
+        return cnt == numCourses ? ans : new int[]{};
     }
 }
