4b4b16a720f0803e0876a00483637787c5571459
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-07 11:51:26 +0530

    Time: 7 ms (57.2%), Space: 47.1 MB (64.8%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,49 +1,94 @@
-//Cycle detection in directed graph using dfs
-class Solution{
-    boolean[] visited;
-    boolean[] pathVis;
-    public boolean canFinish(int numCourses, int[][] prerequisites){
-        
+//Kahn's Algo(cycle detection in DAG usinf bfs)
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
-        
-        visited = new boolean[numCourses];
-        pathVis = new boolean[numCourses];
 
-        for(int i = 0 ; i < numCourses ; i++){
+        for (int i = 0; i < numCourses; i++) {
             adj.add(new ArrayList<>());
         }
 
-        for(int[] edge : prerequisites){
+        for (int[] edge : prerequisites) {
             int u = edge[0];
             int v = edge[1];
 
             adj.get(u).add(v);
         }
 
-        for(int i = 0 ; i < numCourses ; i++){
-            if(dfs(i, adj)) return false; //means cycle exists
-        }
-        return true; //no cycle exists
-    }
-
-    public boolean dfs(int node, List<List<Integer>> adj){
-        visited[node] = true;
-        pathVis[node] = true;
-
-        for(Integer it : adj.get(node)){
-            if(!visited[it]){
-                if(dfs(it, adj)) return true;
-            }
-            else if(pathVis[it]){
-                return true;
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            for (Integer it : adj.get(i)) {
+                indegree[it]++;
             }
         }
-        pathVis[node] = false;
-        return false;
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.offer(i);
+        }
+
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.poll();
+
+            cnt++;
+
+            for (Integer it : adj.get(node)) {
+                indegree[it]--;
+
+                if (indegree[it] == 0)
+                    q.offer(it);
+            }
+        }
+        return cnt == numCourses; //means all nodes are covered so, no cycle exists.
     }
 }
 
+//Cycle detection in directed graph using dfs
+// class Solution{
+//     boolean[] visited;
+//     boolean[] pathVis;
+//     public boolean canFinish(int numCourses, int[][] prerequisites){
 
+//         List<List<Integer>> adj = new ArrayList<>();
+
+//         visited = new boolean[numCourses];
+//         pathVis = new boolean[numCourses];
+
+//         for(int i = 0 ; i < numCourses ; i++){
+//             adj.add(new ArrayList<>());
+//         }
+
+//         for(int[] edge : prerequisites){
+//             int u = edge[0];
+//             int v = edge[1];
+
+//             adj.get(u).add(v);
+//         }
+
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(dfs(i, adj)) return false; //means cycle exists
+//         }
+//         return true; //no cycle exists
+//     }
+
+//     public boolean dfs(int node, List<List<Integer>> adj){
+//         visited[node] = true;
+//         pathVis[node] = true;
+
+//         for(Integer it : adj.get(node)){
+//             if(!visited[it]){
+//                 if(dfs(it, adj)) return true;
+//             }
+//             else if(pathVis[it]){
+//                 return true;
+//             }
+//         }
+//         pathVis[node] = false;
+//         return false;
+//     }
+// }
 
 //Topological sort kahn's algo(BFS)
 // class Solution {
