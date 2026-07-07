9353980cef3e8180ecc6e72ee70bbdabf8e8dbaf
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-07 17:37:58 +0530

    Time: 4 ms (94.85%), Space: 46.8 MB (66.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,6 +1,9 @@
-class Solution{
+class Solution {
+    boolean[] visited;
+    boolean[] pathVis;
+    Stack<Integer> st;
+
     public int[] findOrder(int numCourses, int[][] prerequisites){
-        
         List<List<Integer>> adj = new ArrayList<>();
 
         for(int i = 0 ; i < numCourses ; i++){
@@ -12,37 +15,99 @@ class Solution{
             int v = edge[1];
 
             adj.get(v).add(u);
-        }       
-
-        int[] indegree = new int[numCourses];
-        for(int i = 0 ; i < numCourses ; i++){
-            for(Integer it : adj.get(i)){
-                indegree[it]++;
-            }
-        } 
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 0 ; i < numCourses ; i++){
-            if(indegree[i] == 0) q.offer(i);
         }
-        
+
+        visited = new boolean[numCourses];
+        pathVis = new boolean[numCourses];
+        st = new Stack<>();
+
         int[] ans = new int[numCourses];
 
-        int cnt = 0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.poll();
-
-            ans[cnt++] = node;
-
-            for(Integer it : adj.get(node)){
-                indegree[it]--;
-
-                if(indegree[it] == 0) q.offer(it); 
+        for(int i = 0 ; i < numCourses ; i++){
+            if(!visited[i]){
+                if(dfs(i, adj)){
+                    return new int[]{};
+                }
             }
         }
 
-        return cnt == numCourses ? ans : new int[]{};
+        int i = 0;
+        while(!st.isEmpty()){
+            ans[i++] = st.pop();
+        }
+
+        return ans;
+    }
+  // returns true if cycle exists
+    public boolean dfs(int node, List<List<Integer>> adj){
+        visited[node] = true;
+        pathVis[node] = true;
+
+        for(Integer it : adj.get(node)){
+            if(!visited[it]){
+                if(dfs(it, adj)){
+                    return true;
+                }
+            }
+            else if(pathVis[it]){
+                return true;
+            }
+        }
+        pathVis[node] = false;
+        st.push(node);
+
+        return false;
     }
 }
+
+
+
+
+// class Solution{
+//     public int[] findOrder(int numCourses, int[][] prerequisites){
+
+//         List<List<Integer>> adj = new ArrayList<>();
+
+//         for(int i = 0 ; i < numCourses ; i++){
+//             adj.add(new ArrayList<>());
+//         }
+
+//         for(int[] edge : prerequisites){
+//             int u = edge[0];
+//             int v = edge[1];
+
+//             adj.get(v).add(u);
+//         }       
+
+//         int[] indegree = new int[numCourses];
+//         for(int i = 0 ; i < numCourses ; i++){
+//             for(Integer it : adj.get(i)){
+//                 indegree[it]++;
+//             }
+//         } 
+
+//         Queue<Integer> q = new LinkedList<>();
+
+//         for(int i = 0 ; i < numCourses ; i++){
+//             if(indegree[i] == 0) q.offer(i);
+//         }
+
+//         int[] ans = new int[numCourses];
+
+//         int cnt = 0;
+//         while(!q.isEmpty()){
+//             int node = q.peek();
+//             q.poll();
+
+//             ans[cnt++] = node;
+
+//             for(Integer it : adj.get(node)){
+//                 indegree[it]--;
+
+//                 if(indegree[it] == 0) q.offer(it); 
+//             }
+//         }
+
+//         return cnt == numCourses ? ans : new int[]{};
+//     }
+// }
