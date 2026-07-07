82bb393f1cfea68803ec830e36197b9b0d008786
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-07 11:43:07 +0530

    Time: 4 ms (91.87%), Space: 47.2 MB (49.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,3 +1,50 @@
+//Cycle detection in directed graph using dfs
+class Solution{
+    boolean[] visited;
+    boolean[] pathVis;
+    public boolean canFinish(int numCourses, int[][] prerequisites){
+        
+        List<List<Integer>> adj = new ArrayList<>();
+        
+        visited = new boolean[numCourses];
+        pathVis = new boolean[numCourses];
+
+        for(int i = 0 ; i < numCourses ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] edge : prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(v);
+        }
+
+        for(int i = 0 ; i < numCourses ; i++){
+            if(dfs(i, adj)) return false; //means cycle exists
+        }
+        return true; //no cycle exists
+    }
+
+    public boolean dfs(int node, List<List<Integer>> adj){
+        visited[node] = true;
+        pathVis[node] = true;
+
+        for(Integer it : adj.get(node)){
+            if(!visited[it]){
+                if(dfs(it, adj)) return true;
+            }
+            else if(pathVis[it]){
+                return true;
+            }
+        }
+        pathVis[node] = false;
+        return false;
+    }
+}
+
+
+
 //Topological sort kahn's algo(BFS)
 // class Solution {
 //     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -35,43 +82,3 @@
 //         return cnt == numCourses;
 //     }
 // }
-
-// Topo Sort - DFS
-class Solution {
-    boolean[] visited;
-    boolean[] pathVis;
-    private boolean dfs(int node, List<List<Integer>> adj){
-        visited[node] = true;
-        pathVis[node] = true;
-        for(Integer it : adj.get(node)){
-            if(!visited[it]){
-                if(dfs(it, adj)) return true;
-            }else if(pathVis[it]){
-                return true;
-            }
-        }
-        pathVis[node] = false;
-        return false;
-    }
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //Create an adjacency list
-        List<List<Integer>> adj = new ArrayList<>();
-        visited = new boolean[numCourses];
-        pathVis = new boolean[numCourses];
-        for(int i = 0; i < numCourses; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i = 0; i < prerequisites.length; i++){
-            int u = prerequisites[i][0];
-            int v = prerequisites[i][1];
-            //v -> u
-            adj.get(v).add(u);
-        }
-        for(int i = 0; i < numCourses; i++){
-            if(!visited[i]){
-                if(dfs(i, adj)) return false; // cycle exists
-            }
-        }
-        return true;
-    }
-}
