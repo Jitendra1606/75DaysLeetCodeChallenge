635db5b4017c683b86799a415c6f0813c1bc19ab
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-18 09:58:41 +0530

    Time: 7 ms (47.48%), Space: 44.1 MB (21.43%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
@@ -3,19 +3,23 @@ class Solution {
         int n = nums.length;
 
         int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
+        for (int i = 0; i < n; i++) {
             int gc = 0;
-            for(int j = i ; j < n ; j++){
+            for (int j = i; j < n; j++) {
                 gc = gcd(gc, nums[j]);
 
-                if(gc == k) cnt++;
+                if (gc == k)
+                    cnt++;
+
+                if (gc < k || gc % k != 0)
+                    break;
             }
         }
         return cnt;
     }
 
-    public int gcd(int a, int b){
-        while(b != 0){
+    public int gcd(int a, int b) {
+        while (b != 0) {
             int temp = b;
             b = a % b;
             a = temp;
@@ -23,3 +27,29 @@ class Solution {
         return a;
     }
 }
+
+// Remember an important property:
+// gcd only stays the same or decreases.
+// It can never increase.
+
+// So if, gc < k
+// then it can never become k later.
+
+// Also, if
+// gc % k != 0
+
+// then it can never become k.
+
+// Example:
+
+// gc = 14
+// k = 6
+
+// Future GCDs are divisors of 14:
+
+// 14
+// 7
+// 2
+// 1
+
+// You'll never reach 6.
