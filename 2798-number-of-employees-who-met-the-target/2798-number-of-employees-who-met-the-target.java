2b50bf1a7f91886ffe2f081ae8cc8ee985c64d86
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-23 18:06:55 +0530

    Time: 0 ms (100%), Space: 44 MB (60.5%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int n = hours.length, cnt = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(hours[i] >= target) cnt++;
+        }
+        return cnt;
+    }
+}
