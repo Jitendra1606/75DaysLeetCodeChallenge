4f184bbbca18bbe8f6370d29fd2e264d92a7afde
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-27 18:10:33 +0530

    Time: 0 ms (100%), Space: 44 MB (8.29%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,26 +1,51 @@
-class Solution{
-    public int findMin(int[] nums){
+class Solution {
+    public int findMin(int[] nums) {
         int n = nums.length;
 
-        if(nums[0] <= nums[n - 1]) return nums[0];
+        if (nums[0] < nums[n - 1])
+            return nums[0];
 
         int low = 0, high = n - 1;
         int ans = Integer.MAX_VALUE;
 
-        while(low <= high){
-
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-            
-            if(nums[low] <= nums[mid]){ //left part is sorted,answer can be here alse
+
+            if (nums[low] <= nums[mid]) { //left part is sorted,answer can be here alse
                 ans = Math.min(ans, nums[low]);
-                low = mid + 1;
-            }else{
-                ans = Math.min(ans, nums[mid]);
+                low = mid + 1; 
+            } else {
+                ans = Math.min(ans, nums[mid]); //right part is sorted,answer can be here alse
                 high = mid - 1;
             }
         }
         return ans;
-    }    
+    }
 }
-//t.c = O(log n)
-//s.c = O(1)
+
+// class Solution{
+//     public int findMin(int[] nums){
+//         int n = nums.length;
+
+//         if(nums[0] <= nums[n - 1]) return nums[0];
+
+//         int low = 0, high = n - 1;
+//         int ans = Integer.MAX_VALUE;
+
+//         while(low <= high){
+
+//             int mid = low + (high - low) / 2;
+
+//             if(nums[low] <= nums[mid]){ //left part is sorted,answer can be here alse
+//                 ans = Math.min(ans, nums[low]);
+//                 low = mid + 1;
+//             }else{
+//                 ans = Math.min(ans, nums[mid]);
+//                 high = mid - 1;
+//             }
+//         }
+//         return ans;
+//     }    
+// }
+// //t.c = O(log n)
+// //s.c = O(1)
