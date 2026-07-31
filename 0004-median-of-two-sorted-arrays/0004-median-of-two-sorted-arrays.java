ca7ea3a7878bc9b7d85b81e97314d64b1afaaa47
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-31 10:47:06 +0530

    Time: 1 ms (100%), Space: 48.7 MB (87.79%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,41 +1,112 @@
 class Solution{
     public double findMedianSortedArrays(int[] nums1, int[] nums2){
-        int n1 = nums1.length , n2 = nums2.length , i = 0 , j = 0;
+        int n1 = nums1.length, n2 = nums2.length;
         int n = n1 + n2;
+
+        int i = 0, j = 0;
+
         int ind1 = n / 2;
         int ind2 = ind1 - 1;
-        int cnt = 0 , indel1 = -1 , indel2 = -1;
+
+        int cnt = 0, indEl1 = -1, indEl2 = -1;
+
         while(i < n1 && j < n2){
+
             if(nums1[i] < nums2[j]){
-                if(cnt == ind1) indel1 = nums1[i];
-                if(cnt == ind2) indel2 = nums1[i];
+
+                if(cnt == ind1) indEl1 = nums1[i];
+                if(cnt == ind2) indEl2 = nums1[i];
+                
                 cnt++;
                 i++;
+
             }else{
-                if(cnt == ind1) indel1 = nums2[j];
-                if(cnt == ind2) indel2 = nums2[j];
+
+                if(cnt == ind1) indEl1 = nums2[j];
+                if(cnt == ind2) indEl2 = nums2[j];
+
                 cnt++;
                 j++;
+
             }
-        } 
+        }
+
         while(i < n1){
-            if(cnt == ind1) indel1 = nums1[i];
-            if(cnt == ind2) indel2 = nums1[i];
+
+            if(cnt == ind1) indEl1 = nums1[i];
+            if(cnt == ind2) indEl2 = nums1[i];
+
             cnt++;
             i++;
         }
+
         while(j < n2){
-            if(cnt == ind1) indel1 = nums2[j];
-            if(cnt == ind2) indel2 = nums2[j];
+
+            if(cnt == ind1) indEl1 = nums2[j];
+            if(cnt == ind2) indEl2 = nums2[j];
+
             cnt++;
             j++;
         }
-        if(n % 2 == 1){
-            return indel1;
-        }
-        return (double)((double)(indel1 + indel2)) / 2.0;
+
+        if(n % 2 == 1) return indEl1;
+
+        return (double)((double)(indEl1 + indEl2)) / 2.0;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+//     public double findMedianSortedArrays(int[] nums1, int[] nums2){
+//         int n1 = nums1.length , n2 = nums2.length , i = 0 , j = 0;
+//         int n = n1 + n2;
+//         int ind1 = n / 2;
+//         int ind2 = ind1 - 1;
+//         int cnt = 0 , indel1 = -1 , indel2 = -1;
+//         while(i < n1 && j < n2){
+//             if(nums1[i] < nums2[j]){
+//                 if(cnt == ind1) indel1 = nums1[i];
+//                 if(cnt == ind2) indel2 = nums1[i];
+//                 cnt++;
+//                 i++;
+//             }else{
+//                 if(cnt == ind1) indel1 = nums2[j];
+//                 if(cnt == ind2) indel2 = nums2[j];
+//                 cnt++;
+//                 j++;
+//             }
+//         } 
+//         while(i < n1){
+//             if(cnt == ind1) indel1 = nums1[i];
+//             if(cnt == ind2) indel2 = nums1[i];
+//             cnt++;
+//             i++;
+//         }
+//         while(j < n2){
+//             if(cnt == ind1) indel1 = nums2[j];
+//             if(cnt == ind2) indel2 = nums2[j];
+//             cnt++;
+//             j++;
+//         }
+//         if(n % 2 == 1){
+//             return indel1;
+//         }
+//         return (double)((double)(indel1 + indel2)) / 2.0;
+//     }
+// }
 
 // class Solution {
 //     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
