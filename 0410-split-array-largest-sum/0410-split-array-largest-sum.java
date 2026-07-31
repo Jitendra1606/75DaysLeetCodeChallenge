3bf0080ba651c78dccbdb2fe8b666998973a23f6
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-07-31 06:06:58 +0530

    Time: 1 ms (55.35%), Space: 42.9 MB (69.87%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.java
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -1,45 +1,105 @@
-class Solution {
-    public int splitArray(int[] nums, int k) {
+class Solution{
+    public int splitArray(int[] nums, int k){
         int n = nums.length;
 
-        int max = 0, sum = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, nums[i]);
-            sum += nums[i];
+        long max = 0, sum = 0;
+        for(int num : nums){
+            max = Math.max(max, num);
+            sum += num;
         }
 
-        int low = max, high = sum;
-        int ans = high;
+        long low = max, high = sum, ans = 0;
+        
+        while(low <= high){
 
-        while (low <= high) {
+            long mid = low + (high - low) / 2;
 
-            int mid = low + (high - low) / 2;
-
-            if (isPossible(nums, mid, k)) {
+            if(isPossible(nums, mid, k)){
                 ans = mid;
                 high = mid - 1;
-            } else {
+            }else{
                 low = mid + 1;
             }
         }
-        return ans;
+        return (int)ans;
     }
 
-    public boolean isPossible(int[] nums, int sub, int k) {
-        int cnt = 1, subSum = 0;
+    public boolean isPossible(int[] nums, long mid, int k){
+        int n = nums.length;
+        long cnt = 1, sum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (subSum + nums[i] > sub) {
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i];
+
+            if(sum > mid){
                 cnt++;
-                subSum = nums[i];
-            } else {
-                subSum += nums[i];
+                sum = nums[i];
             }
-            if (cnt > k)
-                return false;
         }
-        return true;
+        return cnt <= k;
     }
 }
-//t.c = O(nlog(sum(nums)))
-//s.c = O(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int splitArray(int[] nums, int k) {
+//         int n = nums.length;
+
+//         int max = 0, sum = 0;
+//         for (int i = 0; i < n; i++) {
+//             max = Math.max(max, nums[i]);
+//             sum += nums[i];
+//         }
+
+//         int low = max, high = sum;
+//         int ans = high;
+
+//         while (low <= high) {
+
+//             int mid = low + (high - low) / 2;
+
+//             if (isPossible(nums, mid, k)) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//     public boolean isPossible(int[] nums, int sub, int k) {
+//         int cnt = 1, subSum = 0;
+
+//         for (int i = 0; i < nums.length; i++) {
+//             if (subSum + nums[i] > sub) {
+//                 cnt++;
+//                 subSum = nums[i];
+//             } else {
+//                 subSum += nums[i];
+//             }
+//             if (cnt > k)
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
+// //t.c = O(nlog(sum(nums)))
+// //s.c = O(1)
