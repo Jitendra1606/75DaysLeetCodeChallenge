1550c15a39a6bb88ff801bff914644e8a5b84e68
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-02 19:30:18 +0530

    Time: 36 ms (79.8%), Space: 98.8 MB (67.5%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,33 +1,83 @@
-class Solution {
-    public long maximumSubarraySum(int[] nums, int k) {
+class Solution{
+    public long maximumSubarraySum(int[] nums, int k){
         int n = nums.length;
+        long ans = 0, sum = 0;
+
         Set<Integer> set = new HashSet<>();
 
-        int i = 0;
-        long ans = 0;
-        long sum = 0;
+        int left = 0, right = 0;
 
-        for(int j = 0 ; j < n ; j++){
+        while(right < n){
 
-            while(set.contains(nums[j])){
-                sum -= nums[i];
-                set.remove(nums[i]);
-                i++;
+            while(set.contains(nums[right])){
+                sum -= nums[left];
+                set.remove(nums[left]);
+                left++;
             }
 
-            sum += nums[j];
-            set.add(nums[j]);
+            sum += nums[right];
 
-            while(j - i + 1 > k){
-                sum -= nums[i];
-                set.remove(nums[i]);
-                i++;
+            set.add(nums[right]);
+
+            while(right - left + 1 > k){
+                sum -= nums[left];
+                set.remove(nums[left]);
+                left++;
             }
 
-            if(j - i + 1 == k){
+            if(right - left + 1 == k){
                 ans = Math.max(ans, sum);
             }
+            
+            right++;
         }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public long maximumSubarraySum(int[] nums, int k) {
+//         int n = nums.length;
+//         Set<Integer> set = new HashSet<>();
+
+//         int i = 0;
+//         long ans = 0;
+//         long sum = 0;
+
+//         for(int j = 0 ; j < n ; j++){
+
+//             while(set.contains(nums[j])){
+//                 sum -= nums[i];
+//                 set.remove(nums[i]);
+//                 i++;
+//             }
+
+//             sum += nums[j];
+//             set.add(nums[j]);
+
+//             while(j - i + 1 > k){
+//                 sum -= nums[i];
+//                 set.remove(nums[i]);
+//                 i++;
+//             }
+
+//             if(j - i + 1 == k){
+//                 ans = Math.max(ans, sum);
+//             }
+//         }
+//         return ans;
+//     }
+// }
