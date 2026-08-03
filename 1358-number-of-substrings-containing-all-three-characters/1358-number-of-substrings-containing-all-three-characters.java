783c32f1c4a1ffc95571499e2bd1d6425a8e4dad
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-03 18:20:46 +0530

    Time: 15 ms (44.62%), Space: 46.3 MB (59.32%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,22 +1,59 @@
-class Solution {
-    public int numberOfSubstrings(String s) {
+class Solution{
+    public int numberOfSubstrings(String s){
         int n = s.length();
-        int ans = 0;
-        int[] arr = new int[3]; 
-        int left = 0;
 
-        for(int i = 0 ; i < n ; i++){
-            char ch = s.charAt(i);
+        int[] arr = new int[3];
+        int left = 0, ans = 0;
+
+        for(int right = 0 ; right < n ; right++){
+            char ch = s.charAt(right);
 
             arr[ch - 'a']++;
 
             while(arr[0] > 0 && arr[1] > 0 && arr[2] > 0){
-                ans += n - i;
+                char c = s.charAt(left);
 
-                arr[s.charAt(left) - 'a']--;
+                arr[c - 'a']--;
+
+                ans += (n - right); // it contains all the possible subarrays
                 left++;
             }
         }
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int n = s.length();
+//         int ans = 0;
+//         int[] arr = new int[3]; 
+//         int left = 0;
+
+//         for(int i = 0 ; i < n ; i++){
+//             char ch = s.charAt(i);
+
+//             arr[ch - 'a']++;
+
+//             while(arr[0] > 0 && arr[1] > 0 && arr[2] > 0){
+//                 ans += n - i;
+
+//                 arr[s.charAt(left) - 'a']--;
+//                 left++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
