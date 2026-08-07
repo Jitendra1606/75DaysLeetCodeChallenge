98ebfa0909258bfdc5bade8d16b3215b531bc4b7
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-07 19:47:53 +0530

    Time: 3 ms (88.44%), Space: 48.3 MB (26.56%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,28 +1,48 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-
-        int n = matrix.length, m = matrix[0].length;
+class Solution{
+    public boolean searchMatrix(int[][] matrix, int target){
+        int n = matrix.length;
+        int m = matrix[0].length;
 
         int row = 0, col = m - 1;
 
         while(row < n && col >= 0){
 
-            if(matrix[row][col] == target){
-                return true;
-            }
+            if(matrix[row][col] == target) return true;
 
-            else if(matrix[row][col] > target){
-                col--;
-            }
+            else if(matrix[row][col] < target) row++;
 
-            else{
-                row++;
-            }
+            else if(matrix[row][col] > target) col--;
         }
-
         return false;
     }
 }
+
+
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+
+//         int n = matrix.length, m = matrix[0].length;
+
+//         int row = 0, col = m - 1;
+
+//         while(row < n && col >= 0){
+
+//             if(matrix[row][col] == target){
+//                 return true;
+//             }
+
+//             else if(matrix[row][col] > target){
+//                 col--;
+//             }
+
+//             else{
+//                 row++;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
 //t.c = O(m + n)
 //s.c = O(1)
 
