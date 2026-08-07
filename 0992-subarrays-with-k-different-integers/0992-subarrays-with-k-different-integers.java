a4ccce5a7f1e060ca9fab4dd68baabd5be3abfaa
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-07 20:02:12 +0530

    Time: 50 ms (40.03%), Space: 50.1 MB (69.44%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,35 +1,67 @@
-class Solution {
+class Solution{
     public int subarraysWithKDistinct(int[] nums, int k) {
-        if(k < 0) return 0;
-        return solve(nums, k) - solve(nums, k - 1);
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 
-    public int solve(int[] nums, int k){
-        int n = nums.length;
+    public int atMost(int[] nums, int x){
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0, sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        int left = 0, right = 0, n = nums.length, sum = 0;
 
         while(right < n){
-
             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
 
-            while(map.size() > k){
-
+            while(map.size() > x){
                 map.put(nums[left], map.get(nums[left]) - 1);
 
-                if(map.get(nums[left]) == 0){
-                    map.remove(nums[left]);
-                }
+                if(map.get(nums[left]) == 0) 
+                map.remove(nums[left]);
 
                 left++;
             }
 
-            sum += (right - left + 1);
+            sum += (right - left + 1); 
 
             right++;
         }
-
         return sum;
     }
 }
+
+
+
+// class Solution {
+//     public int subarraysWithKDistinct(int[] nums, int k) {
+//         if(k < 0) return 0;
+//         return solve(nums, k) - solve(nums, k - 1);
+//     }
+
+//     public int solve(int[] nums, int k){
+//         int n = nums.length;
+
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         int left = 0, right = 0, sum = 0;
+
+//         while(right < n){
+
+//             map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+
+//             while(map.size() > k){
+
+//                 map.put(nums[left], map.get(nums[left]) - 1);
+
+//                 if(map.get(nums[left]) == 0){
+//                     map.remove(nums[left]);
+//                 }
+
+//                 left++;
+//             }
+
+//             sum += (right - left + 1);
+
+//             right++;
+//         }
+
+//         return sum;
+//     }
+// }
