575cce92c0f4263759ac28d4ace0bf936541d19f
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-08 07:05:29 +0530

    Time: 9 ms (92.28%), Space: 46.2 MB (93.62%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int calculate(String s) {
+        int n = s.length();
+
+        Stack<Integer> st = new Stack<>();
+
+        int res = 0, curr = 0, sign = 1;
+
+        for(char ch : s.toCharArray()){
+            
+            if(Character.isDigit(ch)){
+                curr = curr * 10 + (ch - '0');
+            }
+            else if(ch == '+'){
+                res += curr * sign;
+                curr = 0;
+                sign = 1;
+            }
+            else if(ch == '-'){
+                res += curr * sign;
+                sign = -1;
+                curr = 0;
+            }
+            else if(ch == '('){
+                st.push(res);
+                st.push(sign);
+                res = 0;
+                sign = 1;
+                curr = 0;
+            }else if(ch == ')'){
+                res += curr * sign;
+                curr = 0;
+                res *= st.pop();
+                res += st.pop();
+            }
+        }
+
+        res += sign * curr;
+        return res;
+    }
+}
