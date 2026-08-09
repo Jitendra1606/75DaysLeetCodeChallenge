0ca7b19ba21e6c789466bc56524ed6a2f1797e1d
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-09 08:19:21 +0530

    Time: 11 ms (41.45%), Space: 46.9 MB (17.37%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.java
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -1,37 +1,76 @@
-class Solution {
-    int n;
-    int[] suffixSum;
-    Integer[][] dp;
-    public int stoneGameII(int[] piles) {
-        n = piles.length;
+class Solution{
+    int[][][] dp = new int[2][102][102];
+    public int stoneGameII(int[] piles){
+        int n = piles.length;
 
-        //suffix sum
-        suffixSum = new int[n];
-
-        suffixSum[n - 1] = piles[n - 1];
-        for(int i = n - 2 ; i >= 0 ; i--){
-            suffixSum[i] = suffixSum[i + 1] + piles[i];
+        for(int[][] arr2d : dp){
+            for(int[] arr1d : arr2d){
+                Arrays.fill(arr1d, -1);
+            }
         }
 
-        dp = new Integer[n][n + 1];
-        
-        return solve(0, 1);
+        return solveForAlice(piles, 1, 0, 1);
     }
 
-    public int solve(int i, int M){
-        if(i >= n) return 0; //no piles left
+    public int solveForAlice(int[] piles, int person, int i, int M){
+        int n = piles.length;
+        if(i >= n) return 0;
 
-        if(i + 2 * M >= n) return suffixSum[i];
+        if(dp[person][i][M] != -1) return dp[person][i][M];
 
-        if(dp[i][M] != null) return dp[i][M];
+        int result = (person == 1) ? -1 : Integer.MAX_VALUE;
+        int stones = 0;
 
-        int maxStones = 0;
+        for(int x = 1 ; x <= Math.min(2 * M, n - i
+        ) ; x++){
+            stones += piles[i + x - 1];
 
-        for(int x = 1 ; x <= 2 * M ; x++){
-            int opponent = solve(i + x, Math.max(M, x));
-
-            maxStones = Math.max(maxStones, suffixSum[i] - opponent);
+            if(person == 1){
+                result = Math.max(result, stones + solveForAlice(piles, 0, i + x, Math.max(M, x)));
+            }else{
+                result = Math.min(result, solveForAlice(piles, 1, i + x, Math.max(M, x)));
+            }
         }
-        return dp[i][M] = maxStones;
+        return dp[person][i][M] = result;
     }
 }
+
+
+
+// class Solution {
+//     int n;
+//     int[] suffixSum;
+//     Integer[][] dp;
+//     public int stoneGameII(int[] piles) {
+//         n = piles.length;
+
+//         //suffix sum
+//         suffixSum = new int[n];
+
+//         suffixSum[n - 1] = piles[n - 1];
+//         for(int i = n - 2 ; i >= 0 ; i--){
+//             suffixSum[i] = suffixSum[i + 1] + piles[i];
+//         }
+
+//         dp = new Integer[n][n + 1];
+        
+//         return solve(0, 1);
+//     }
+
+//     public int solve(int i, int M){
+//         if(i >= n) return 0; //no piles left
+
+//         if(i + 2 * M >= n) return suffixSum[i];
+
+//         if(dp[i][M] != null) return dp[i][M];
+
+//         int maxStones = 0;
+
+//         for(int x = 1 ; x <= 2 * M ; x++){
+//             int opponent = solve(i + x, Math.max(M, x));
+
+//             maxStones = Math.max(maxStones, suffixSum[i] - opponent);
+//         }
+//         return dp[i][M] = maxStones;
+//     }
+// }
