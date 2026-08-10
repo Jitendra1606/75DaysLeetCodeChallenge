38cb6ee0478a326771f5c7d790eef3d510278bde
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-08-10 13:53:53 +0530

    Time: 10 ms (79.4%), Space: 49.4 MB (35.04%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,54 +1,61 @@
-class Pair{
-    int distance;
-    int node;
-
-    Pair(int distance, int node){
-        this.distance = distance;
-        this.node = node;
-    }
-}
-
+//we are implementing Dijsktra's here to find the shortest path to all nodes from k
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         List<List<Pair>> adj = new ArrayList<>();
-        for(int i = 0 ; i <= n ; i++){
+
+        for (int i = 0; i <= n; i++) {
             adj.add(new ArrayList<>());
         }
-        for(int i = 0 ; i < times.length ; i++){
-            int u = times[i][0];
-            int v = times[i][1];
-            int wt = times[i][2];
 
-            adj.get(u).add(new Pair(wt, v));
+        for (int[] time : times) {
+            int u = time[0];
+            int v = time[1];
+            int weight = time[2];
+
+            adj.get(u).add(new Pair(weight, v));
         }
+
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, (int) (1e9));
+
+        dist[k] = 0;
+
         PriorityQueue<Pair> pq = new PriorityQueue<Pair>((x, y) -> x.distance - y.distance);
-
-        int[] distance = new int[n + 1];
-        Arrays.fill(distance, (int)1e9);
-
-        distance[k] = 0;
         pq.offer(new Pair(0, k)); //{distance, node}
 
-        while(!pq.isEmpty()){
-            Pair it = pq.poll();
-            int d = it.distance;
-            int node = it.node;
+        while (!pq.isEmpty()) {
+            Pair cur = pq.poll();
 
-            for(Pair p : adj.get(node)){
-                int wt = p.distance;
-                int adjNode = p.node;
+            int d = cur.distance;
+            int node = cur.node;
 
-                if(d + wt < distance[adjNode]){
-                    distance[adjNode] = d + wt;
-                    pq.offer(new Pair(distance[adjNode], adjNode));
+            for (Pair p : adj.get(node)) {
+                int dis = p.distance;
+                int newNode = p.node;
+
+                if (dist[newNode] > dis + d) {
+                    dist[newNode] = d + dis;
+
+                    pq.offer(new Pair(d + dis, newNode));
                 }
             }
         }
         int max = 0;
-        for(int i = 1 ; i <= n ; i++){
-            if(distance[i] == (int)1e9) return -1;
-            max = Math.max(max, distance[i]);
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == (int) 1e9)
+                return -1;
+            max = Math.max(max, dist[i]);
         }
         return max;
+    }
+}
+
+class Pair {
+    int distance;
+    int node;
+
+    Pair(int distance, int node) {
+        this.distance = distance;
+        this.node = node;
     }
 }
