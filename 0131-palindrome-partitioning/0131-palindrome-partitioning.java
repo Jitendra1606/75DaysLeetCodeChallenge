3f2038ad8d2b964a6b3d4a8dd0013ec55b5984af
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-01 14:45:48 +0530

    Time: 8 ms (74.58%), Space: 65 MB (87.8%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -2,24 +2,23 @@ class Solution{
     public List<List<String>> partition(String s){
         List<List<String>> result = new ArrayList<>();
 
-        solve(s, 0, new ArrayList<>(), result);
+        solve(0, s, new ArrayList<>(), result);
+
         return result;
     }
 
-    public void solve(String s, int ind, List<String> list, List<List<String>> result){
-
+    public void solve(int ind, String s, List<String> list, List<List<String>> result){
         if(ind == s.length()){
-
             result.add(new ArrayList<>(list));
             return;
         }
 
         for(int i = ind ; i < s.length() ; i++){
-
             if(isPalindrome(s, ind, i)){
-
                 list.add(s.substring(ind, i + 1));
-                solve(s, i + 1, list, result);
+
+                solve(i + 1, s, list, result);
+
                 list.remove(list.size() - 1);
 
             }
@@ -27,15 +26,62 @@ class Solution{
     }
 
     public boolean isPalindrome(String s, int i, int j){
-        
-        while(i < j){
-
+        while(i <= j){
             if(s.charAt(i) != s.charAt(j)) return false;
-            i++; 
+
+            i++;
             j--;
         }
         return true;
     }
 }
-//t.c = O(n^2.2^n)
-//s.c = O(n)
+
+
+
+
+
+
+
+
+
+// class Solution{
+//     public List<List<String>> partition(String s){
+//         List<List<String>> result = new ArrayList<>();
+
+//         solve(s, 0, new ArrayList<>(), result);
+//         return result;
+//     }
+
+//     public void solve(String s, int ind, List<String> list, List<List<String>> result){
+
+//         if(ind == s.length()){
+
+//             result.add(new ArrayList<>(list));
+//             return;
+//         }
+
+//         for(int i = ind ; i < s.length() ; i++){
+
+//             if(isPalindrome(s, ind, i)){
+
+//                 list.add(s.substring(ind, i + 1));
+//                 solve(s, i + 1, list, result);
+//                 list.remove(list.size() - 1);
+
+//             }
+//         }
+//     }
+
+//     public boolean isPalindrome(String s, int i, int j){
+        
+//         while(i < j){
+
+//             if(s.charAt(i) != s.charAt(j)) return false;
+//             i++; 
+//             j--;
+//         }
+//         return true;
+//     }
+// }
+// //t.c = O(n^2.2^n)
+// //s.c = O(n)
