9f82490f2309bf4efbcd396c9de4e82031e852bf
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-02 06:53:21 +0530

    Time: 1 ms (31.89%), Space: 45.3 MB (35.83%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+
+//Always possible because, if all elements of array is even then ok,
+// if all elements of array are odd then also ok
+// and now if there present even a single odd or single even then
+//we can simply make an array of odd's in that case
