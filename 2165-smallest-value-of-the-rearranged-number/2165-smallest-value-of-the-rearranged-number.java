b1fcf1f8bb590534857e624a03c85e1438a5ee0f
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-03 10:40:10 +0530

    Time: 2 ms (84.62%), Space: 42.6 MB (53.57%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.java
@@ -0,0 +1,58 @@
+class Solution {
+    public long smallestNumber(long num) {
+        if(num == 0) return 0;
+        
+        if (num < 0) {
+            char[] nums = String.valueOf(num).toCharArray();
+
+            Arrays.sort(nums);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = nums.length - 1; i > 0; i--) {
+                sb.append(nums[i]);
+            }
+
+            long ans = -Long.parseLong(sb.toString());
+            return ans;
+        } else {
+            char[] nums = String.valueOf(num).toCharArray();
+
+            Arrays.sort(nums);
+
+            StringBuilder sb = new StringBuilder();
+
+            int zeroCnt = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == '0')
+                    zeroCnt++;
+            }
+
+            if (zeroCnt == 0) {
+                for (int i = 0; i < nums.length; i++) {
+                    sb.append(nums[i]);
+                }
+
+                Long ans = Long.parseLong(sb.toString());
+                return ans;
+            }
+
+            else {
+                int i = 0;
+                while (nums[i] == '0')
+                    i++;
+
+                char temp = nums[i];
+                nums[i] = nums[0];
+                nums[0] = temp;
+
+                for (int k = 0; k < nums.length; k++) {
+                    sb.append(nums[k]);
+                }
+
+                Long ans = Long.parseLong(sb.toString());
+                return ans;
+            }
+        }
+    }
+}
