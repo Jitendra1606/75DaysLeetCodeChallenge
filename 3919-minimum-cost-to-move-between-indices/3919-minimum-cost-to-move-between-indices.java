3b49f9359b0630c0fb29ecccbe1479f817611632
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-04 15:18:09 +0530

    Time: 6 ms (74.68%), Space: 185.9 MB (73.42%) - LeetHub

--- 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.java
+++ 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.java
@@ -2,51 +2,105 @@ class Solution {
     public int[] minCost(int[] nums, int[][] queries) {
         int n = nums.length;
 
-        int[] forward = new int[n - 1];
+        int[] close = new int[n];
+        close[0] = 1;
 
-        int[] backward = new int[n - 1];
-
-        for(int i = 0 ; i < n - 1 ; i++){
-
-            int diff = nums[i + 1] - nums[i];
-
-            //i -> i + 1, forward
-            if(i == 0 || diff < nums[i] - nums[i - 1]){
-                forward[i] = 1;
-            }else{
-                forward[i] = diff;
-            }
-
-            //i + 1 -> i
-            if(i + 1 == n - 1 || diff <= nums[i + 2] - nums[i + 1]){
-                backward[i] = 1;
-            }else{
-                backward[i] = diff;
+        for (int i = 1; i < n - 1; i++) {
+            if (Math.abs(nums[i] - nums[i - 1]) <= Math.abs(nums[i] - nums[i + 1])) {
+                close[i] = i - 1;
+            } else {
+                close[i] = i + 1;
             }
         }
+        close[n - 1] = n - 2;
 
-        //prefix sum
-        int[] prefor = new int[n];
-        int[] prebac = new int[n];
+        int[] forward = new int[n];
+        for (int i = 1 ; i < n; i++) {
+            if (i == close[i - 1])
+                forward[i] = 1;
 
-        for(int i = 0 ; i < n - 1 ; i++){
-            prefor[i + 1] = prefor[i] + forward[i];
+            else
+                forward[i] = Math.abs(nums[i] - nums[i - 1]);
 
-            prebac[i + 1] = prebac[i] + backward[i]; 
+            forward[i] = forward[i] + forward[i - 1];
+        }
+
+        int[] backward = new int[n];
+        for (int i = n - 2; i >= 0; i--) {
+            if (i == close[i + 1])
+                backward[i] = 1;
+
+            else
+                backward[i] = Math.abs(nums[i] - nums[i + 1]);
+
+            backward[i] = backward[i + 1] + backward[i];
         }
 
         int[] ans = new int[queries.length];
 
-        for(int i = 0 ; i < queries.length ; i++){
-            int start = queries[i][0];
-            int end = queries[i][1];
+        for (int i = 0; i < queries.length; i++) {
+            int l = queries[i][0];
+            int r = queries[i][1];
 
-            if(start < end){
-                ans[i] = prefor[end] - prefor[start];
-            }else{
-                ans[i] = prebac[start] - prebac[end];
+            if (l < r) {
+                ans[i] = forward[r] - forward[l];
+            } else {
+                ans[i] = backward[r] - backward[l];
             }
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public int[] minCost(int[] nums, int[][] queries) {
+//         int n = nums.length;
+
+//         int[] forward = new int[n - 1];
+
+//         int[] backward = new int[n - 1];
+
+//         for(int i = 0 ; i < n - 1 ; i++){
+
+//             int diff = nums[i + 1] - nums[i];
+
+//             //i -> i + 1, forward
+//             if(i == 0 || diff < nums[i] - nums[i - 1]){
+//                 forward[i] = 1;
+//             }else{
+//                 forward[i] = diff;
+//             }
+
+//             //i + 1 -> i
+//             if(i + 1 == n - 1 || diff <= nums[i + 2] - nums[i + 1]){
+//                 backward[i] = 1;
+//             }else{
+//                 backward[i] = diff;
+//             }
+//         }
+
+//         //prefix sum
+//         int[] prefor = new int[n];
+//         int[] prebac = new int[n];
+
+//         for(int i = 0 ; i < n - 1 ; i++){
+//             prefor[i + 1] = prefor[i] + forward[i];
+
+//             prebac[i + 1] = prebac[i] + backward[i]; 
+//         }
+
+//         int[] ans = new int[queries.length];
+
+//         for(int i = 0 ; i < queries.length ; i++){
+//             int start = queries[i][0];
+//             int end = queries[i][1];
+
+//             if(start < end){
+//                 ans[i] = prefor[end] - prefor[start];
+//             }else{
+//                 ans[i] = prebac[start] - prebac[end];
+//             }
+//         }
+//         return ans;
+//     }
+// }
