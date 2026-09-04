998e9a8e4f0125b429ca7515b77db2b6f5c85926
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-04 19:40:21 +0530

    Time: 4 ms (29.8%), Space: 101.4 MB (82.43%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -1,26 +1,80 @@
-class Solution {
-    public int waysToSplitArray(int[] nums) {
+class Solution{
+    public int waysToSplitArray(int[] nums){
         int n = nums.length;
 
         long[] pre = new long[n];
         pre[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            pre[i] = nums[i] + pre[i - 1];
+
+        for(int i = 1 ; i < n ; i++){
+            pre[i] = pre[i - 1] + nums[i];
         }
 
-        long[] suff = new long[n];
-        suff[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suff[i] = nums[i] + suff[i + 1];
+        int ways = 0;
+        for(int i = 0 ; i < n - 1 ; i++){
+            long left = pre[i];
+            long right = pre[n - 1] - pre[i];
+
+            if(left >= right) ways++;
         }
 
-        //calculating total points
-        long ways = 0;
-        for (int i = 0; i < n; i++) {
-            if (i + 1 < n)
-                if (pre[i] >= suff[i + 1])
-                    ways++;
-        }
-        return (int)ways;
+        return ways;
     }
 }
+
+
+// class Solution{
+//     public int waysToSplitArray(int[] nums){
+//         int n = nums.length;
+
+//         long[] pre = new long[n];
+//         pre[0] = nums[0];
+
+//         for(int i = 1 ; i < n ; i++){
+//             pre[i] = nums[i] + pre[i - 1];
+//         }
+
+//         int ways = 0;
+
+//         for(int i = 0 ; i < n - 1 ; i++){
+//             long left = pre[i];
+//             long right = pre[n - 1] - pre[i];
+
+//             if(left >= right) ways++;
+//         }
+//         return ways;
+//     }
+// }
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int waysToSplitArray(int[] nums) {
+//         int n = nums.length;
+
+//         long[] pre = new long[n];
+//         pre[0] = nums[0];
+//         for (int i = 1; i < n; i++) {
+//             pre[i] = nums[i] + pre[i - 1];
+//         }
+
+//         long[] suff = new long[n];
+//         suff[n - 1] = nums[n - 1];
+//         for (int i = n - 2; i >= 0; i--) {
+//             suff[i] = nums[i] + suff[i + 1];
+//         }
+
+//         //calculating total points
+//         long ways = 0;
+//         for (int i = 0; i < n; i++) {
+//             if (i + 1 < n)
+//                 if (pre[i] >= suff[i + 1])
+//                     ways++;
+//         }
+//         return (int)ways;
+//     }
+// }
