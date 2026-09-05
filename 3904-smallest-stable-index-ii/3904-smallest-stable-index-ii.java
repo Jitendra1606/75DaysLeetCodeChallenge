c5762fc9e8a220599c9b3ffb4f7f6c7a29fc12e6
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-05 08:31:28 +0530

    Time: 5 ms (66.41%), Space: 126.6 MB (82.44%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,21 +1,50 @@
-class Solution {
-    public int firstStableIndex(int[] nums, int k) {
+class Solution{
+    public int firstStableIndex(int[] nums, int k){
         int n = nums.length;
+
         int[] maxi = new int[n];
-        int[] mini = new int[n];
-        int curmax = nums[0];
+        int currMax = nums[0];
+
         for(int i = 0 ; i < n ; i++){
-            curmax = Math.max(curmax, nums[i]);
-            maxi[i] = curmax;
+            currMax = Math.max(currMax, nums[i]);
+            maxi[i] = currMax;
         }
-        int curmin = nums[n - 1];
+
+        int[] mini = new int[n];
+        int currMin = nums[n - 1];
+
         for(int i = n - 1 ; i >= 0 ; i--){
-            curmin = Math.min(curmin, nums[i]);
-            mini[i] = curmin;
+            currMin = Math.min(currMin, nums[i]);
+            mini[i] = currMin;
         }
+
         for(int i = 0 ; i < n ; i++){
             if(maxi[i] - mini[i] <= k) return i;
         }
+
         return -1;
     }
 }
+
+
+// class Solution {
+//     public int firstStableIndex(int[] nums, int k) {
+//         int n = nums.length;
+//         int[] maxi = new int[n];
+//         int[] mini = new int[n];
+//         int curmax = nums[0];
+//         for(int i = 0 ; i < n ; i++){
+//             curmax = Math.max(curmax, nums[i]);
+//             maxi[i] = curmax;
+//         }
+//         int curmin = nums[n - 1];
+//         for(int i = n - 1 ; i >= 0 ; i--){
+//             curmin = Math.min(curmin, nums[i]);
+//             mini[i] = curmin;
+//         }
+//         for(int i = 0 ; i < n ; i++){
+//             if(maxi[i] - mini[i] <= k) return i;
+//         }
+//         return -1;
+//     }
+// }
