bef65a85fa450afcf61abd25066e0d2a0301ab8c
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-05 11:12:33 +0530

    Time: 30 ms (52.4%), Space: 208.7 MB (24.62%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,21 +1,21 @@
-class Solution {
-    boolean[] visited;
+class Solution{
     int min;
-    public int minScore(int n, int[][] roads) {
+    boolean[] visited;
 
+    public int minScore(int n, int[][] roads){
         min = Integer.MAX_VALUE;
         visited = new boolean[n + 1];
-
+    
         List<List<int[]>> adj = new ArrayList<>();
 
         for(int i = 0 ; i <= n ; i++){
             adj.add(new ArrayList<>());
         }
 
-        for(int[] road : roads){
-            int u = road[0];
-            int v = road[1];
-            int d = road[2];
+        for(int[] edge : roads){
+            int u = edge[0];
+            int v = edge[1];
+            int d = edge[2];
 
             adj.get(u).add(new int[]{v, d});
             adj.get(v).add(new int[]{u, d});
@@ -30,14 +30,76 @@ class Solution {
 
         for(int[] mat : adj.get(node)){
             int city = mat[0];
-            int dis = mat[1];
+            int dist = mat[1];
 
-            min = Math.min(min, dis);
+            min = Math.min(min, dist);
 
             if(!visited[city]){
                 dfs(city, adj);
-                visited[city] = true;
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     boolean[] visited;
+//     int min;
+//     public int minScore(int n, int[][] roads) {
+
+//         min = Integer.MAX_VALUE;
+//         visited = new boolean[n + 1];
+
+//         List<List<int[]>> adj = new ArrayList<>();
+
+//         for(int i = 0 ; i <= n ; i++){
+//             adj.add(new ArrayList<>());
+//         }
+
+//         for(int[] road : roads){
+//             int u = road[0];
+//             int v = road[1];
+//             int d = road[2];
+
+//             adj.get(u).add(new int[]{v, d});
+//             adj.get(v).add(new int[]{u, d});
+//         }
+
+//         dfs(1, adj);
+//         return min;
+//     }
+
+//     public void dfs(int node, List<List<int[]>> adj){
+//         visited[node] = true;
+
+//         for(int[] mat : adj.get(node)){
+//             int city = mat[0];
+//             int dis = mat[1];
+
+//             min = Math.min(min, dis);
+
+//             if(!visited[city]){
+//                 dfs(city, adj);
+//                 visited[city] = true;
+//             }
+//         }
+//     }
+// }
