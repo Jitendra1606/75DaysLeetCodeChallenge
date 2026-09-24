56119c364a3ccee904f0a80135b1519064ab77e5
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-24 08:20:39 +0530

    Time: 1 ms (99.69%), Space: 45.2 MB (95.27%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.java
@@ -2,21 +2,17 @@ class Solution {
     public int smallestIndex(int[] nums) {
         int n = nums.length;
 
-        for(int i = 0 ; i < n ; i++){
-            if(isdig(nums[i], i)) return i;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            while (nums[i] != 0) {
+                int rem = nums[i] % 10;
+                sum += rem;
+                nums[i] /= 10;
+            }
+            if (sum == i)
+                return i;
         }
         return -1;
-    }
-
-    public boolean isdig(int dig, int tar){
-        int sum = 0;
-
-        while(dig != 0){
-            int rem = dig % 10;
-            sum += rem;
-            dig /= 10;
-        }
-
-        return sum == tar;
     }
 }
