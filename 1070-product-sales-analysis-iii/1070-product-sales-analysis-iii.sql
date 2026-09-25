8704746ceb66417afaac9eec2108fddc843e41c4
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-25 11:29:49 +0530

    Time: 767 ms (55.45%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -4,7 +4,7 @@ year AS first_year,
 quantity,
 price
 FROM Sales
-WHERE(product_id, year) IN (
+WHERE (product_id, year) IN (
     SELECT product_id, MIN(year)
     FROM Sales
     GROUP BY product_id
