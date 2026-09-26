4d24b11fd9c47ec10beffe824336b99532356c2d
Jitendra1606 <jitendrasharma01606@gmail.com>
2026-09-26 18:35:47 +0530

    Time: 33 ms (85.52%), Space: 90.6 MB (86.42%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,12 +1,13 @@
-class Solution {
-    public String evaluate(String s, List<List<String>> knowledge) {
+class Solution{
+    public String evaluate(String s, List<List<String>> knowledge){
         Map<String, String> map = new HashMap<>();
 
         for(int i = 0 ; i < knowledge.size() ; i++){
             map.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
         }
-        
+
         StringBuilder sb = new StringBuilder();
+
         for(int i = 0 ; i < s.length() ; i++){
             
             if(s.charAt(i) == '('){
@@ -25,14 +26,55 @@ class Solution {
                 else{
                     sb.append("?");
                 }
-                
-                i = j;
 
-            }else{
+                i = j;
+            }
+            else{
                 sb.append(s.charAt(i));
             }
         }
-
         return sb.toString();
     }
 }
+
+
+
+
+// class Solution {
+//     public String evaluate(String s, List<List<String>> knowledge) {
+//         Map<String, String> map = new HashMap<>();
+
+//         for(int i = 0 ; i < knowledge.size() ; i++){
+//             map.put(knowledge.get(i).get(0), knowledge.get(i).get(1));
+//         }
+        
+//         StringBuilder sb = new StringBuilder();
+//         for(int i = 0 ; i < s.length() ; i++){
+            
+//             if(s.charAt(i) == '('){
+                
+//                 int j = i + 1;
+
+//                 while(s.charAt(j) != ')'){
+//                     j++;
+//                 }
+
+//                 String t = s.substring(i + 1, j);
+
+//                 if(map.containsKey(t)){
+//                     sb.append(map.get(t));
+//                 }
+//                 else{
+//                     sb.append("?");
+//                 }
+                
+//                 i = j;
+
+//             }else{
+//                 sb.append(s.charAt(i));
+//             }
+//         }
+
+//         return sb.toString();
+//     }
+// }
